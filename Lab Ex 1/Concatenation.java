855d45edc97c769e636f92676010334c82953ddf
Sharmila2104 <69077796+Sharmila2104@users.noreply.github.com>
2020-08-02 17:09:28 +0530

    Add files via upload

---
+++ Lab Ex 1/Concatenation.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package concatenation;
+import java.util.Scanner;
+
+/**
+ *
+ * @author sharmila
+ */
+public class Concatenation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        System.out.println("enter the number");
+        int a=obj.nextInt();
+        String s;
+        for(int i=1;i<=a;i++)
+        {
+            s=" "+i;
+        System.out.print(s);
+    }
+    }
+}
+                    
+        
+        
+        
+    
+    
+
